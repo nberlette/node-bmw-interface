6ac6dae37437c1bb5acfc71359633151d4420e66
Ken Malinich <kmalinich@users.noreply.github.com>
2025-06-23 15:59:53 -0400

    Update

---
+++ eslint.globals.mjs
@@ -0,0 +1,24 @@
+const projectGlobals = {
+	api         : false,
+	app_intf    : false,
+	app_name    : false,
+	app_path    : false,
+	bitmask     : false,
+	bus         : false,
+	config      : true,
+	hex         : false,
+	host_data   : false,
+	intf        : false,
+	json        : false,
+	log         : false,
+	num         : false,
+	objfmt      : false,
+	proto       : false,
+	socket      : false,
+	status      : true,
+	terminating : false,
+	update      : false,
+};
+
+
+export default projectGlobals;
